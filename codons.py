b6bea66b7191842b58a9aff9a9309e277ed5fafb
YUVALSARUSI1 <yuval4.2000@gmail.com>
2025-11-27 14:10:30 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,15 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
+    file_path = "data/codons.txt" 
+    f = open(file_path, "r")
+    lines = f.readlines()
+    f.close
+    dict = {} 
+    for l in lines:
+        dict += l.strip()
+        lines.split()
+        cells = l.find_all("td")
+        key = cells[0].text
+        value = cells[3].text
+        dict[] = value
+    return dict
 
